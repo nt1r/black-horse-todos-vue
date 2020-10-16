6ddc6ab7df7f564d7592cecc1f30f8878017785b
Leqi <michaelleqihust@gmail.com>
2020-10-16 11:33:04 +0800

    [leqi] feat「#17」：使用Vue Cli搭建项目脚手架。 1. TypeScript 2. Vue 3. babel 4. eslint 5. jest 6. sass

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Todos</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TodoList extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ tasking.md
@@ -0,0 +1,6 @@
+# Tasking
+
+## Card 17
+
+- 使用@vue/cli搭建脚手架
+- 更改模版输出为hello world
